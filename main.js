c3ac645ab14872b6d29c88287a637677fd0ee88c
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-05-31 23:59:53 +0200

    Added script movingMenu

--- main.js
+++ main.js
@@ -3,38 +3,22 @@ $(document).ready(initAll);
 function initAll() {
   /*tu umieszczajmy wywołania*/
   atfHeight();
+  movingMenu();
 }
 
 function atfHeight(){
-  $('#sec1-atf-container').height($(window).innerHeight());
+  $('#header-section').height($(window).innerHeight());
 }
 
-$(document).ready(function() {
-  // Add smooth scrolling to all links
-  $("a").on("click", function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function() {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-});
+function movingMenu(){
+jQuery('.menu-button').click(function(event){
+       event = event || window.event;
+       var sectionID = event.currentTarget.id + "-section";
+       jQuery("html,body").animate({
+           scrollTop: jQuery("#" + sectionID).offset().top
+       }, 1000)
+   });
+}
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
