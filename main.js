ca931b27f8b7b6cdf11b74496e62b0dd5942d7f4
AdamReszka <adam.reszka85@gmail.com>
2018-06-07 18:02:57 +0200

    add services top layer

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function initAll() {
   /*tu umieszczajmy wywołania*/
   atfHeight();
   movingMenu();
+  servicesLayer();
 }
 
 function atfHeight(){
@@ -27,4 +28,36 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+}
+
+function servicesLayer() {
+  $('.layer-open').each(function() {
+    $(this).on('click', function() {
+      $(this).closest('body').find('#services-list-layer').removeClass('fadeOut');
+      $(this).closest('body').find('#services-list-layer').addClass('open');
+    $(this).closest('body').find('#services-list-layer').addClass('fadeIn');
+    });
+  });
+  $('.sl-layer-cover').on('click', function(e) {
+    if (e.target !== this) {
+      return;
+    } else {
+      if($('#services-list-layer').hasClass('open')) {
+        $('#services-list-layer').removeClass('fadeIn');
+        $('#services-list-layer').addClass('fadeOut');
+        setTimeout(function(){
+          $('#services-list-layer').removeClass('open');
+        }, 500);
+      }
+    }
+  });
+  $('.temporary-close-button').on('click', function() {
+    $('#services-list-layer').removeClass('fadeIn');
+    $('#services-list-layer').addClass('fadeOut');
+    /*$('#services-layer-container').addClass('fadeOutUp');*/
+    setTimeout(function(){
+      $('#services-list-layer').removeClass('open');
+      /*$('#services-layer-container').removeClass('fadeOutUp');*/
+    }, 1000);
+  });
 }
