eb177f3c027ef5a96bce163906ef9946692c13aa
AdamReszka <adam.reszka85@gmail.com>
2018-07-15 21:52:57 +0200

    add almost completely acordeon animations function

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function initAll() {
   atfHeight();
   movingMenu();
   servicesLayer();
-  acoMovements();
   burgerMenu();
   closeBurger();
   upToDate();
@@ -56,12 +55,37 @@ function rellax() {
 }
 
 function servicesLayer() {
-  $('.layer-open').each(function() {
-    $(this).on('click', function() {
+  var categories = [];
+  var heights = [];
+  var firstClick = true;
+  $('.layer-open').each(function(i) {
+    $(this).on('click', function(event) {
       $(this).closest('body').find('#services-list-layer').removeClass('fadeOut');
       $(this).closest('body').find('#services-list-layer').addClass('open');
-    $(this).closest('body').find('#services-list-layer').addClass('fadeIn');
+      $(this).closest('body').find('#services-list-layer').addClass('fadeIn');
+
+      //----------------------początek testu wysokości
+      $('.acordeon-category-container').each(function(i){
+        var catNum = i + 1;
+        var catName = "serviceCategory" + catNum;
+        $(this).addClass(catName);
+        var catHeight = $(this).find('.acordeon-category-header').height() + $(this).find('.category-sublist').height();
+        if(firstClick == true) {
+          categories.push(catName);
+          heights.push(catHeight);
+        }
+      });
+      event = event || window.event;
+      var targetCat = "#" + event.currentTarget.id + "-container";
+      $(targetCat).addClass('open');
+      var containerClass = categories[i];
+      var containerHeight = heights[i];
+      $('.'+containerClass).css('height',containerHeight+'px')
+      firstClick = false;
+      //------------------------koniec testu wysokości
+
     });
+
   });
   $('.sl-layer-cover').on('click', function(e) {
     if (e.target !== this) {
@@ -72,9 +96,41 @@ function servicesLayer() {
         $('#services-list-layer').addClass('fadeOut');
         setTimeout(function(){
           $('#services-list-layer').removeClass('open');
+          $('.acordeon-category-container').each(function() {
+            $(this).removeClass('open');
+          });
         }, 500);
       }
     }
+  });
+
+/*  $('.layer-open').each(function(i) {
+    $(this).on('click', function(event) {
+      ;
+    });
+  }); */
+
+  $('.acordeon-open-arrow').each(function(j) {
+    $(this).on('click', function() {
+      var containerClass2 = categories[j];
+      var containerHeight2 = heights[j];
+      console.log('naciśnięto strzałkę '+j);
+      console.log('adam ' + categories + " " + heights);
+      console.log('adam ' + categories[j] + " " + heights[j]);
+      console.log('adam ' + containerClass2 + " " + containerHeight2);
+      if ($(this).closest('.acordeon-category-container').hasClass('open')) {
+        $(this).closest('.acordeon-category-container').css('height','6rem');
+        $(this).closest('.acordeon-category-container').removeClass('open');
+      } else {
+        $('.acordeon-category-container').each(function() {
+          $(this).css('height','6rem');
+          $(this).removeClass('open');
+          $(this).closest('.acordeon-categories').find('.'+containerClass2).css('height',containerHeight2+'px');
+        });
+
+        $(this).closest('.acordeon-category-container').addClass('open');
+      }
+    });
   });
   $('.acordeon-close-button').on('click', function() {
     $('#services-list-layer').removeClass('fadeIn');
@@ -88,28 +144,7 @@ function servicesLayer() {
       /*$('#services-layer-container').removeClass('fadeOutUp');*/
     }, 1000);
   });
-}
 
-function acoMovements() {
-  $('.acordeon-open-arrow').each(function() {
-    $(this).on('click', function() {
-      if ($(this).closest('.acordeon-category-container').hasClass('open')) {
-        $(this).closest('.acordeon-category-container').removeClass('open');
-      } else {
-        $('.acordeon-category-container').each(function() {
-          $(this).removeClass('open');
-        });
-        $(this).closest('.acordeon-category-container').addClass('open');
-      }
-    });
-  });
-  $('.layer-open').each(function() {
-    $(this).on('click', function(event) {
-      event = event || window.event;
-      var targetCat = "#" + event.currentTarget.id + "-container";
-      $(targetCat).addClass('open');
-    });
-  });
 }
 
 function upToDate() {
